d810ed3e59184eabf00356c9c2ac4af10768aaef
Erick Luan <flinprice1308ft@gmail.com>
2022-04-15 07:45:58 -0300

    Hello Word

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import { Home } from "./pages/Home";
+import "./styles/global.scss"
 
 export function App() {
   return (
     <div className="App">
-      <h1>Olá! Mundo</h1>
+      <Home />
     </div>
   );
 }
